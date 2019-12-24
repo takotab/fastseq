dc70cadc873c326abe30f8401528c00bc66e6a55
tako_desktop <takotabak@gmail.com>
2019-12-24 16:26:45 +0100

    Getting the M4 dataset

--- get_data.py
+++ get_data.py
@@ -1,14 +1,23 @@
-import os 
-from fastcore import fastcore
+import os
+
+# from fastcore import fastcore
 import wget
 
+
 def main():
-    path = Path('data')
-    
-    if not os.path.exists('data/Daily-train.csv'):
-        wget.download('https://raw.githubusercontent.com/M4Competition/M4-methods/master/Dataset/Train/Daily-train.csv',path)
-    if not os.path.exists('data/Daily-test.csv'):
-        wget.download('https://raw.githubusercontent.com/M4Competition/M4-methods/master/Dataset/Test/Daily-test.csv',path)
+    # path = Path('data')
+
+    if not os.path.exists("data/Daily-train.csv"):
+        wget.download(
+            "https://raw.githubusercontent.com/M4Competition/M4-methods/master/Dataset/Train/Daily-train.csv",
+            "m4_daily/train.csv",
+        )
+    if not os.path.exists("data/Daily-test.csv"):
+        wget.download(
+            "https://raw.githubusercontent.com/M4Competition/M4-methods/master/Dataset/Test/Daily-test.csv",
+            "m4_daily/test.csv",
+        )
+
 
 if __name__ == "__main__":
     main()

613772423c36765bbd7d6aa36d5be353e4ff234d
tako_desktop <takotabak@gmail.com>
2020-01-04 10:22:16 +0100

    started with the preproccesing functions

--- fastseq/_nbdev.py
+++ fastseq/_nbdev.py
@@ -2,11 +2,12 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"hello": "00_core.ipynb",
+index = {"ts_lists": "00_core.ipynb",
+         "melted_ts_2_lists": "00_core.ipynb",
          "m4_base": "01_data.external.ipynb",
          "URLs.m4_daily": "01_data.external.ipynb",
-         "TimeSeriesDataset": "03_data.load.ipynb",
          "pad_zeros": "03_data.load.ipynb",
+         "TimeSeriesDataset": "03_data.load.ipynb",
          "encodes": "04_data.transforms.ipynb",
          "decodes": "04_data.transforms.ipynb",
          "ConcreteDropout": "05_models.wavenet.ipynb",
@@ -17,6 +18,8 @@ modules = ["core.py",
            "data/load.py",
            "data/transforms.py",
            "models/wavenet.py"]
+
+doc_url = "https://takotab.github.io/fastseq/"
 
 git_url = "https://github.com/takotab/fastseq/tree/master/"
 

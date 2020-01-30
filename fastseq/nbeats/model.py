4e165c71db438e66b18feaf98785801788b5ba13
tako_vaio <takotabak@gmail.com>
2020-01-30 14:49:58 +0100

    note to fix diff in xb and total_b

--- fastseq/nbeats/model.py
+++ fastseq/nbeats/model.py
@@ -231,5 +231,6 @@ class NBeatsTrainer(Callback):
 
     def after_pred(self):
         self.pred[2]['total_b'] = self.pred[1]
+        # TODO add total_b_loss to self.out
         self.out = concat_dct(self.pred[2], self.out)
         self.learn.pred = self.pred[0]

e165b1105a7ffbce9bd512c25942785f98c22033
tako_desktop <takotabak@gmail.com>
2020-02-10 14:07:08 +0100

    thetas for trend seems to do well.
    season still needs some work

--- fastseq/data/external.py
+++ fastseq/data/external.py
@@ -13,7 +13,7 @@ m4_base = "https://motionnet-m4-dataset.s3.eu-central-1.amazonaws.com/"
 URLs.m4_daily = f'{m4_base}m4_daily.tgz'
 
 # Cell
-def dummy_data_generator(lookback:int, horizon:int, signal_type='seasonality', nrows:int=5, random = True, batch_size=32, norm=False):
+def dummy_data_generator(lookback:int, horizon:int, signal_type='seasonality', nrows:int=5, random = True, batch_size=32, norm=False, noise = .2):
     def get_datapoint():
         lin_space = np.linspace(-lookback, horizon, lookback + horizon)
         if random:
@@ -22,23 +22,23 @@ def dummy_data_generator(lookback:int, horizon:int, signal_type='seasonality', n
             offset = 1
         if signal_type == 'lin':
             a =  np.random.standard_normal() * lin_space + offset * 100
-        if signal_type == 'trend':
+
+        a = np.zeros_like(lin_space)
+        if signal_type is not 'seasonality':
             p = 4
             a = [(lin_space**(i))[None,:] for i in range(p)]
             T = np.concatenate(a)
             thetas = np.random.randn(4)*1*9**-p
             a= np.matmul(thetas,T)
 
-        elif signal_type == 'seasonality':
-            a = np.cos(2 * np.random.randint(low=1, high=3) * np.pi * lin_space)* np.random.standard_normal() * .5
+        if signal_type is not 'trend':
+            a += np.cos(2 * np.random.randint(low=1, high=3) * np.pi * lin_space)* np.random.standard_normal() * .5
             a += np.cos(2 * np.random.randint(low=2, high=4) * np.pi * lin_space)* np.random.standard_normal() * .5
             a += np.sin(2 * np.random.randint(low=1, high=3) * np.pi * lin_space)
             a -= np.sin(2 * np.random.randint(low=2, high=4) * np.pi * lin_space)
             a += lin_space * offset + np.random.rand() * 10
-        elif signal_type == 'cos':
-            a = np.cos(2 * np.pi * lin_space)
-        else:
-            raise Exception('Unknown signal type.')
+
+        a += np.random.randn(a.shape[0])*noise
         if norm:
             return (a[None,:]-a.mean())/a.std()
         else:
